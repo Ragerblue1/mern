1abbfb8a2852af93dd3606190dfcde3e1c5da85f
ASHISH KASHYAP <167999545+Ragerblue1@users.noreply.github.com>
2024-07-25 19:51:36 +0530

    Update ListProduct.jsx

--- Admin/src/Components/ListProduct/ListProduct.jsx
+++ Admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('https://mern-backend-djlj.onrender.com') 
+    fetch('https://mern-backend-djlj.onrender.com/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
@@ -16,7 +16,7 @@ const ListProduct = () => {
     }, [])
 
     const removeProduct = async (id) => {
-      await fetch('https://mern-backend-djlj.onrender.com', {
+      await fetch('https://mern-backend-djlj.onrender.com/removeproduct', {
       method: 'POST',
       headers: {
         Accept:'application/json',
@@ -25,7 +25,7 @@ const ListProduct = () => {
       body: JSON.stringify({id:id}),
     })
 
-    fetch('https://mern-backend-djlj.onrender.com') 
+    fetch('https://mern-backend-djlj.onrender.com/allproducts') 
     .then((res) => res.json()) 
     .then((data) => setAllProducts(data))
 
