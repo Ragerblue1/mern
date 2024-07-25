cc8e37b606d738fd44ebb1860c22dd1f66cc3125
ASHISH KASHYAP <167999545+Ragerblue1@users.noreply.github.com>
2024-07-25 13:17:47 +0530

    Add files via upload

---
+++ Frontend/src/Pages/Product.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import Breadcrums from '../Components/Breadcrums/Breadcrums'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+import MoreProducts from '../Components/MoreProducts/MoreProducts'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../Context/ShopContext'
+
+const Product = () => {
+  const {products} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = products.find((e)=>e.id === Number(productId));
+  return (
+    <div>
+      <Breadcrums product={product}/>
+      <ProductDisplay product={product}/>
+      <DescriptionBox/>
+      <MoreProducts/>
+    </div>
+  )
+}
+
+export default Product
