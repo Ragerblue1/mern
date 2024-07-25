919763876e2e773262714ea8fbed89089e85011f
ASHISH KASHYAP <167999545+Ragerblue1@users.noreply.github.com>
2024-07-25 19:45:23 +0530

    Update ShopCategory.jsx

--- Frontend/src/Pages/ShopCategory.jsx
+++ Frontend/src/Pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ const ShopCategory = (props) => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('https://mern-backend-djlj.onrender.com') 
+    fetch('https://mern-backend-djlj.onrender.com/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
